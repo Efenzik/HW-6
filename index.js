605833cf92acdfef6c750b9848cf655ed30b6e9e
Artem Cherkashin <157517719+Efenzik@users.noreply.github.com>
2025-02-10 18:58:23 +0200

    end

---
+++ index.js
@@ -0,0 +1,46 @@
+"use strict";
+// 1 ============
+
+// let counter = 1;
+// while (counter <= 10) {
+//   console.log(counter);
+//   counter += 1;
+// }
+
+// 2 ============
+
+// for (let i = 1; i <= 20; i++) {
+//   if (i % 2 !== 0) {
+//     continue;
+//   }
+//   console.log(i);
+// }
+
+// 3 ============
+
+// const seven = 7;
+
+// for (let i = 1; i <= 10; i++) {
+//   console.log(seven * i);
+// }
+
+// 4 ============
+// const n = Number(prompt("n = "));
+
+// for (let i = 1; ; i++) {
+//   if (i >= n) {
+//     break;
+//   }
+//   console.log(i);
+// }
+
+// 5 ============
+// let i = 0;
+
+// while (i <= 20) {
+//   i += 1;
+//   if (i % 3 === 0) {
+//     continue;
+//   }
+//   console.log(i);
+// }
